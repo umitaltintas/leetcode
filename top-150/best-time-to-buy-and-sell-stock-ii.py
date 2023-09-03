68000b883f71490b2fd26cd1606b997d8bcdfca6
umit altintas <umitaltintas1@gmail.com>
2023-09-03 19:15:44 +0300

    feat: Add solution for "Best Time to Buy and Sell Stock II" problem and unit tests

---
+++ top-150/best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,45 @@
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        if not prices:
+            return 0
+
+        min_price = prices[0]
+        max_profit = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            else:
+                max_profit += prices[i] - min_price
+                min_price = prices[i]
+
+        return max_profit
+
+
+import unittest
+
+
+class TestSolution(unittest.TestCase):
+    def test_maxProfit_emptyList(self):
+        s = Solution()
+        self.assertEqual(s.maxProfit([]), 0)
+
+    def test_maxProfit_singlePrice(self):
+        s = Solution()
+        self.assertEqual(s.maxProfit([7]), 0)
+
+    def test_maxProfit_increasingPrices(self):
+        s = Solution()
+        self.assertEqual(s.maxProfit([1, 2, 3, 4, 5]), 4)
+
+    def test_maxProfit_decreasingPrices(self):
+        s = Solution()
+        self.assertEqual(s.maxProfit([5, 4, 3, 2, 1]), 0)
+
+    def test_maxProfit_mixedPrices(self):
+        s = Solution()
+        self.assertEqual(s.maxProfit([7, 1, 5, 3, 6, 4]), 7)
+
+
+if __name__ == "__main__":
+    unittest.main()
