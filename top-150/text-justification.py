35d02f33e43fcdec10e3b0aca6395a5086a80202
umitaltintas <umitaltintas1@gmail.com>
2023-09-15 00:47:33 +0300

    feat: Add text justification solution
    Add a new solution for text justification problem in the top-150 list. The solution includes a `fullJustify` method that takes a list of words and a maximum line width, and returns a list of justified lines. The method uses a utility function `addWordsToLine` to format words to a given line width. The solution is implemented in the `text-justification.py` file.

---
+++ top-150/text-justification.py
@@ -0,0 +1,38 @@
+class Solution:
+    def fullJustify(self, words: list[str], maxWidth: int) -> list[str]:
+
+        def addWordsToLine(words, spaces, isLastLine=False):
+            """Utility function to format words to a given line width."""
+            if isLastLine or len(words) == 1:
+                return " ".join(words).ljust(maxWidth, " ")
+
+            justified_line = []
+            for i, word in enumerate(words):
+                justified_line.append(word)
+                if i < len(words) - 1:
+                    justified_line.append(" " * spaces[i])
+            return "".join(justified_line)
+
+        # Group words into lines
+        lines, line, line_length = [], [], 0
+        for word in words:
+            if line_length + len(word) + len(line) > maxWidth:
+                lines.append((line, maxWidth - line_length))
+                line, line_length = [], 0
+            line.append(word)
+            line_length += len(word)
+        lines.append((line, maxWidth - line_length))
+
+        # Justify the lines
+        justified_lines = []
+        for i, (line_words, spaces_left) in enumerate(lines):
+            if i == len(lines) - 1 or len(line_words) == 1:
+                justified_lines.append(addWordsToLine(line_words, [spaces_left], True))
+            else:
+                base_spaces = spaces_left // (len(line_words) - 1)
+                extra_spaces = spaces_left % (len(line_words) - 1)
+                spaces = [base_spaces + 1] * extra_spaces + [base_spaces] * (len(line_words) - 1 - extra_spaces)
+                justified_lines.append(addWordsToLine(line_words, spaces))
+        return justified_lines
+
+
